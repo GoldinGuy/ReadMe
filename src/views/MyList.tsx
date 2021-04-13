fb11b33593bc42a715ae403c72e00c56062f94ea
Seth <sethgoldin27@gmail.com>
2021-04-13 13:56:28 -0400

    improve reading list ux

--- src/views/MyList.tsx
+++ src/views/MyList.tsx
@@ -26,17 +26,18 @@ const MyListPage = () => {
 		<div className="bg-greener-50 text-center py-10 w-screen overflow-x-hidden ">
 			<div className="pb-10 text-center md:text-lg w-screen sm:w-10/12 lg:w-9/12 overflow-hidden break-words flex  items-center m-auto justify-between">
 				<button
-					className="rounded-md h-8 bg-greener-400 text-md md:text-md text-greener hover:text-greener-dark font-bold focus:outline-none focus:border-0 self-center"
+					className="rounded-md h-8 bg-greener-400 text-md md:text-md text-greener hover:text-greener-dark font-bold focus:outline-none focus:border-0 self-center pr-8"
 					title="Share"
 				>
 					<FontAwesomeIcon icon={faShare} size={"lg"} />
+					<span className="block">Share</span>
 				</button>
-				<i className="text-center">
+				<i className="text-center font-semibold text-grayest">
 					{QUOTES[Math.floor(Math.random() * QUOTES.length)]}
 				</i>
 				{/* Your Reading List Has Been Generated! */}
 				<button
-					className=" rounded-md h-8 bg-greener-400 text-md md:text-md text-greener hover:text-greener-dark font-bold focus:outline-none focus:border-0 self-center"
+					className="pl-8 rounded-md h-8 bg-greener-400 text-md md:text-md text-greener hover:text-greener-dark font-bold focus:outline-none focus:border-0 self-center"
 					title="Download"
 					onClick={async () => {
 						var readingListString = "";
@@ -44,8 +45,8 @@ const MyListPage = () => {
 							readingListString = readingListString.concat(
 								idx + 1 + `) ${book.title} By: ${book.author}\n`
 							);
+							return readingListString;
 						});
-						console.log(readingListString);
 						downloadReadingList(
 							readingListString,
 							"text/plain",
@@ -54,22 +55,23 @@ const MyListPage = () => {
 					}}
 				>
 					<FontAwesomeIcon icon={faDownload} size={"lg"} />
+					<span className="block">Download</span>
 				</button>
 			</div>
 			{LIST.map((book: RecBook, idx: number) => {
 				return (
 					<button
-						className="bg-white pl-0 h-20 w-11/12 sm:w-9/12 lg:w-7/12 rounded-md hover:bg-greener-lightest m-auto relative mb-1 focus:outline-none focus:border-0 grid "
+						className="bg-white pl-0 h-20 w-11/12 sm:w-9/12 lg:w-7/12 rounded-md hover:bg-greener-lightest m-auto relative mb-1 focus:outline-none focus:border-0 flex justify-between items-center text-grayest"
 						key={idx + "-btn"}
 					>
-						<span className="align-middle self-center" key={idx + "-span"}>
-							<span className="flex flex-col pl-14 sm:pl-0 self-center">
-								<img
-									key={book.tags[0] + "-img-" + book.title}
-									alt="book-cover"
-									className="h-20 w-14 float-left top-0 left-0 rounded-md absolute"
-									src={book.image_url}
-								/>
+						<img
+							key={book.tags[0] + "-img-" + book.title}
+							alt="book-cover"
+							className="h-20 w-14 rounded-md "
+							src={book.image_url}
+						/>
+						<span className="px-6" key={idx + "-span"}>
+							<span className="flex-col">
 								<a
 									href={book.url}
 									target="_blank"
@@ -80,7 +82,7 @@ const MyListPage = () => {
 									{book.title}
 								</a>
 								<span
-									className="text-sm"
+									className="block text-sm"
 									key={book.title + "-author-" + book.title}
 								>
 									{book.author}
@@ -89,14 +91,15 @@ const MyListPage = () => {
 						</span>
 
 						<a
-							className="float-right absolute right-0 top-6 mr-3 "
-							href={`https://www.amazon.com/books/s?k=${encodeURIComponent(
+							className="mr-3 text-grayest"
+							href={book.url}
+							/* 	TODO: Amazon href={`https://www.amazon.com/books/s?k=${encodeURIComponent(
 								book.title
-							)}+book`}
+							)}+book`} */
 							target="_blank"
 							rel="noreferrer"
 						>
-							<FontAwesomeIcon icon={faExternalLinkAlt} />
+							<FontAwesomeIcon icon={faExternalLinkAlt} size={"lg"} />
 						</a>
 					</button>
 				);
